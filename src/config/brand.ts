b30dba96ab90f58061907314fc4f357a46059d62
Saurabh Pandey <saurabhpandey081220@gmail.com>
2026-06-04 10:58:11 +0530

    Delete src/config directory

--- src/config/brand.ts
+++
@@ -1,7 +0,0 @@
-export const BRAND = {
-  name: 'Sembark',
-  tagline: 'Travel Software',
-  logo: `${process.env.PUBLIC_URL || ''}/sembark-logo.png`,
-  logoAlt: 'Sembark Logo',
-  website: 'https://sembark.com/',
-};
