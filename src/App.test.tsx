90f8b29f75ba9470cc836671eef951f227f797d6
Saurabh Pandey <saurabhpandey081220@gmail.com>
2026-06-04 10:57:23 +0530

    Delete src/App.test.tsx

--- src/App.test.tsx
+++
@@ -1,7 +0,0 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
-
-test('renders Sembark logo', async () => {
-  render(<App />);
-  expect(screen.getByAltText('Sembark Logo')).toBeInTheDocument();
-});
